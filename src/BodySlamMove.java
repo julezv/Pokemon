79138505445a253af7fe9317bf7bc45725f7950e
Yulia Volkova <yuulia.volkova@gmail.com>
2019-03-19 23:28:35 +0000

    - added 30% and paralyze effect

--- src/BodySlamMove.java
+++ src/BodySlamMove.java
@@ -3,9 +3,13 @@ import ru.ifmo.se.pokemon.*;
 /**
  * Created by yuliav on 04/03/2019.
  */
-// Body Slam deals damage and has a 30% chance of paralyzing the target.
-// Electric type Pokémon, those with the
-// ability Limber or those behind a Substitute cannot be paralyzed.
+
+/**
+ * Body Slam deals damage and has a 30% chance of paralyzing the target.
+ * Electric type Pokémon, those with the
+ * ability Limber or those behind a Substitute cannot be paralyzed.
+ */
+
 
 //MA: TODO: 30% + paralize Effect
 public class BodySlamMove extends PhysicalMove{
@@ -14,10 +18,8 @@ public class BodySlamMove extends PhysicalMove{
     }
     @Override
     protected void applyOppEffects(Pokemon p){
-        Effect paralyzeByChance = new Effect().chance(0.33);
-        paralyzeByChance.paralyze(p);
-        // ?
-        // why can do p.paralyze() and Effect.paralyze(p)
+        Effect paralyzeByChance = new Effect().condition(Status.PARALYZE).chance(0.3);
+        p.setCondition(paralyzeByChance);
     }
     @Override
     protected String describe() {
