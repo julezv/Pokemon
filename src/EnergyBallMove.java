b5f89558042d6201d19ece9aef3ea52c078706e1
Yulia Volkova <yuulia.volkova@gmail.com>
2019-03-19 23:57:17 +0000

    - Add javadocs with moves description - Add for Energy Ball: 10% chance of lowering the target's Special Defense by one stage. - Add for Thunder move 30% chance to paralyze

--- src/EnergyBallMove.java
+++ src/EnergyBallMove.java
@@ -1,9 +1,12 @@
-import ru.ifmo.se.pokemon.SpecialMove;
-import ru.ifmo.se.pokemon.Type;
+import ru.ifmo.se.pokemon.*;
 
 /**
  * Created by yuliav on 03/03/2019.
  */
+/*
+* Energy Ball deals damage and has a 10% chance of
+* lowering the target's Special Defense by one stage.
+* */
 
 //MA: TODO: Energy Ball deals damage and has a 10% chance of lowering the target's Special Defense by one stage.
 public class EnergyBallMove extends SpecialMove {
@@ -16,4 +19,9 @@ public class EnergyBallMove extends SpecialMove {
         return "Attack with Energy Ball Move";
     }
 
+    @Override
+    protected void applyOppEffects(Pokemon p){
+        Effect LowerSpecialDefense = new Effect().chance(0.1).stat(Stat.SPECIAL_DEFENSE, -1);;
+        p.setCondition(LowerSpecialDefense);
+    }
 }
--- src/SwaggerMove.java
+++ src/SwaggerMove.java
@@ -4,8 +4,11 @@ import ru.ifmo.se.pokemon.*;
  * Created by yuliav on 03/03/2019.
  */
 
-// Effect: Raises the target's Attack by two stages, then confuses it. If the target's Attack cannot be raised by two stages,
-// the confusion is not applied.
+/**
+ * Effect: Raises the target's Attack by two stages, then confuses it. If the target's Attack cannot be raised by two stages,
+ * the confusion is not applied.
+ */
+
 
 
 public class SwaggerMove extends StatusMove {
