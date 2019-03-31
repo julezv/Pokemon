d023f9dfc4ad2cc19bf23ddeef5d4f292240c542
Yulia Volkova <yuulia.volkova@gmail.com>
2019-03-31 23:31:08 +0100

    - increase probability of critical hit

--- src/StoneEdgeMove.java
+++ src/StoneEdgeMove.java
@@ -21,7 +21,19 @@ public class StoneEdgeMove extends PhysicalMove{
     protected double calcCriticalHit(Pokemon att,
                                      Pokemon def){
         //TODO: need to redefine
-        if(att.getStat(Stat.SPEED) / 512.0D > Math.random()) {
+
+        //alpha ( = Math.random() ) in U[0; 1)
+        //P(alpha in [0; x]) = x
+        //P(att.getStat(Stat.SPEED) / 512.0D > alpha) = P(alpha in [0; att.getStat(Stat.SPEED) / 512.0D)) =
+        //= att.getStat(Stat.SPEED) / 512.0D
+        //int a = 5;
+        //int b = 3;
+        //((double)a) / b
+        //(a * 1.0) / b
+        //a / b * 1.0;
+
+
+        if((1.0 / 8) > Math.random()) {
             return 2.0D;
         } else {
             return 1.0D;
